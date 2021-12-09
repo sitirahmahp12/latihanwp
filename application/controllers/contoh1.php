f1e840598e562d12797500e3d2e7b68b4935fea4
sitirahmap12 <fadilahrahmah858@gmail.com>
2021-12-09 16:51:42 +0700

    pertemuan 2

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class contoh1 extends CI_Controller 
+{
+	public function index ()
+	{
+
+	    echo "<h1>perkenalkan</h1>";
+	    echo "nama saya siti rahmah padilah
+	          saya tinggal di daerah bogor 
+	          olahraga yang saya sukai adalah renang ";
+    
+    }
+}
---
+++ application/views/view-latihan.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+   <title>latihan1</title>
+
+</head>
+
+<body>
+
+       hallo kawan... yuk kita belajar web pronggramming ..!!!<br>
+       nllai 1 = <?= $nilai1; ?>
+       nilai 2 = <?= $nilai2; ?>
+       ini hasil dari pemodeelan dengan methode penjulahan yaitu <?=  $nilai1. " + " . $nilai2 . " = " . $hasil; ?>
+
+</body>
+
+</html>
