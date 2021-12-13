8f639901d1003ada8787e62bebba1425482f097d
sitirahmap12 <fadilahrahmah858@gmail.com>
2021-12-13 15:52:05 +0700

    pertemuan 3

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>nama </h4>
+ <ul type="disc">
+ <li>Nama Depan : siti rahmah </li>
+ <li>Nama Belakang : padilah </li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> jln balimbing v ujung </li>
+ </ul>
+
+ <h4>tempat lahir</h4>
+ <ul type="none">
+ <li>bogor</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>renang </li>
+ </ul>
+</section>
